8cf94aac4418639930455b6fb8607f4b2fe2a37a
Robert Frank <robert.allan.frank@gmail.com>
2019-10-20 13:50:23 -0500

    Increase code coverage

--- tests/integration/helpers/array-splice-test.js
+++ tests/integration/helpers/array-splice-test.js
@@ -13,4 +13,20 @@ module('Integration | Helper | array-splice', function(hooks) {
     await render(hbs`{{array-splice array 1 0 'February'}}`);
     assert.equal(this.element.textContent.trim(), 'January,February,March,April,June');
   });
+
+  test('can ensure delete count is provided', async function(assert) {
+    const array = ['January', 'March', 'April', 'June'];
+    this.set('array', array);
+
+    await render(hbs`{{array-splice array}}`);
+    assert.equal(this.element.textContent.trim(), '');
+  });
+
+  test('can splice elements with a deleted value', async function(assert) {
+    const array = ['January', 'February', 'March'];
+    this.set('array', array);
+
+    await render(hbs`{{array-splice array 1 1}}`);
+    assert.equal(this.element.textContent.trim(), 'January,March');
+  });
 });
